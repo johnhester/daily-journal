c74e17abbb5ca9e99656cfbd7f3b656eeca5ead1
John Hester <hesterusmaximus@gmail.com>
2020-05-29 15:17:37 -0500

    minor updates

---
+++ src/scripts/journal.js
@@ -0,0 +1,24 @@
+// journal entry object
+
+const journalEntry = {
+    journalDate: 2020-05-29 ,
+    journalConcepts: "booty",
+    journalEntry: "Today......",
+    mood: "😀"
+}
+
+// entry storage array
+const entries =[
+
+];
+
+// entry retreival function
+const getEntry = () => {
+
+}
+
+// entry storage function
+
+const storeEntry = (entry) => {
+    entries.push(entry);
+}
