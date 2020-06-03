573cf59b6c3712ac4478063e16f589fa81ab239f
John Hester <hesterusmaximus@gmail.com>
2020-06-02 22:57:12 -0500

    werk werk werk

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,7 +1,14 @@
 // Journal entry converter object -> html
 const entryConverter = (journalObject) => {
     const entryHTMLRep = `
-    
+    <section class="prevEntry">
+        <div class="prevEntry__banner">
+            <div class="prevEntry__banner--element">${journalObject.journaldate}</div>
+            <div class="prevEntry__banner--element">Concepts: ${journalObject.journalConcepts}</div>
+            <div class="prevEntry__banner--element">Mood: ${journalObject.mood}</div>
+        </div>
+        <p class="prevEntry__text">${journalObject.journalEntry}</p>
+    </section>
     `
     return entryHTMLRep;
 }
