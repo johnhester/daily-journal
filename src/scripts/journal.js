a0679a7f37d743d735f74a3edb8dcf0be8ea4af1
John Hester <hesterusmaximus@gmail.com>
2020-06-02 22:49:35 -0500

    css/js

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,24 +1,7 @@
-// journal entry object
-
-const journalEntry = {
-    journalDate: 2020-05-29 ,
-    journalConcepts: "booty",
-    journalEntry: "Today......",
-    mood: "😀"
-}
-
-// entry storage array
-const entries =[
-
-];
-
-// entry retreival function
-const getEntry = () => {
-
-}
-
-// entry storage function
-
-const storeEntry = (entry) => {
-    entries.push(entry);
+// Journal entry converter object -> html
+const entryConverter = (journalObject) => {
+    const entryHTMLRep = `
+    
+    `
+    return entryHTMLRep;
 }
