3253eadc1741a1086e66b5fa2c929fadae017274
John Hester <hesterusmaximus@gmail.com>
2020-06-14 19:41:26 -0500

    grab and store entry data attempt

--- src/scripts/entryGrabber.js
+++ src/scripts/entryGrabber.js
@@ -1,4 +1,20 @@
+import API from "./saveJournalEntry"
+
 // grabs new entry data 
 
-const buttonEntry = document.getElementById("myJournalButton").value 
-console.log(buttonEntry)
+
+
+document.querySelector("#myJournalButton").addEventListener("click", event => {
+
+    const journalEntryObj = {}
+
+    journalEntryObj.date = document.querySelector("#journalDate").value
+    console.log("date", journalEntryObj.date)
+    journalEntryObj.concepts = document.querySelector("#journalConcepts").value
+    journalEntryObj.entry = document.querySelector("#journalEntry").value
+    journalEntryObj.mood = document.querySelector("#mood").value
+
+    console.log(journalEntryObj)
+
+    API.saveJournalEntry(journalEntryObj)
+})
